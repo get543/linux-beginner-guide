0b986345c0cf23c84252e314e80bf3acc7fb9668
Udin <fghaza.pratama@gmail.com>
2022-10-09 15:46:07 +0700

    Update change.sh
    - change port easier with adding "port" in the second argument
    - current mode display in zenity
    - now display sound_card_number in the terminal

--- change.sh
+++ change.sh
@@ -1,18 +1,31 @@
 #! /bin/bash
 
-sound_card_number=2
-sink_port=3
+sound_card_number=1 || $sound_card_number
+sink_port=3 || $sink_port
 
 title="Change Output Device"
-prompt="Pick your mode :"
+prompt="Status : unknown $prompt"
+column="Choose What Mode"
 options=("Speaker Mode" "Headphone Mode")
 
-while opt=$(zenity --title="$title" --text="$prompt" --list --column="Options" "${options[@]}")
+if [[ $1 == port ]]
+then
+    echo -ne "Please put the sound card number : "
+    read sound_card_number
+    echo -ne "Please put the sink port : "
+    read sink_port
+fi
+
+while opt=$(zenity --title="$title" --text="$prompt" --list --column="$column" "${options[@]}")
 do
     case "$opt" in
     "${options[0]}")
         clear
 
+        echo -e "\033[1;31m\n\t\tLooking For Sound Card Number \033[0m"
+        aplay --list-devices
+
+        echo -e "\033[1;31m\n\t\tStatus Auto Mute Mode (Item0:) \033[0m"
         amixer -c $sound_card_number set 'Auto-Mute Mode' Disabled
 
         echo -e "\033[1;31m\n\t\tLooking For Sink Port \033[0m"
@@ -22,10 +35,15 @@ do
 
         echo -e "\nStatus :\033[1;31m\t\tSpeaker Mode \033[0m"
         zenity --notification --window-icon="info" --text="You are now on '$opt'"
+        prompt="Status : Speaker Mode"
         ;;
     "${options[1]}")
         clear
 
+        echo -e "\033[1;31m\n\t\tLooking For Sound Card Number \033[0m"
+        aplay --list-devices
+
+        echo -e "\033[1;31m\n\t\tStatus Auto Mute Mode (Item0:) \033[0m"
         amixer -c $sound_card_number set 'Auto-Mute Mode' Enabled
 
         echo -e "\033[1;31m\n\t\tLooking For Sink Port \033[0m"
@@ -35,6 +53,7 @@ do
 
         echo -e "\nStatus :\033[1;31m\t\tHeadphone Mode \033[0m"
         zenity --notification --window-icon="info" --text="You are now on '$opt'"
+        prompt="Status : Headphone Mode"
         ;;
     *)
         zenity --notification --window-icon="error" --text="Invalid option. You didn't choose any of the options."
