d7c73002d79e9d26a18b5d318cd150ab8304a0f3
get543 <fghaza.pratama@gmail.com>
2023-03-13 20:32:53 +0700

    Choose upgrade now supports fedora and arch (planning for opensuse)
    - Flip the check nala command, the true condition is at the top (less confusion).
    - On debian, if nala is installed, now nala is use for upgrading packages.
    - Also on debian, if you don't have nala, using apt but adding update repo then install.
    - Adding support for fedora, using update repo command, and the upgrade command.
    - Needs testing on arch, but it's working on english based system.
    - Support arch system, using system-uprade & force no answer, and using install command.
    - Update the other distros' message.
    - Add comment which indicates where the script starts.

--- autoinstall/updates/upgrade.sh
+++ autoinstall/updates/upgrade.sh
@@ -23,7 +23,8 @@ upgradeALL() {
 }
 
 chooseUpgrade() {
-  if [[ "$debian" ]]
+  # debian
+  if [ "$debian" ]
   then
     while :
     do
@@ -31,12 +32,12 @@ chooseUpgrade() {
 
       echo -e "\n${DMagenta}============= List Upgradable Packages =============${Color_Off}"
 
-      # check nala
-      if ( ! command -v nala &> /dev/null )
+      # using nala or apt
+      if ( command -v nala &> /dev/null )
       then
-        apt list --upgradable
-      else
         nala list --upgradable
+      else
+        apt list --upgradable
       fi
 
       echo -e "\n${DMagenta}=====================================================================================================${Color_Off}"
@@ -57,22 +58,100 @@ chooseUpgrade() {
         break
       fi
 
-      echo -e "\n${Green}Back to using ${BRed}apt ${Green}because nala doesn't support this process.${Color_Off}\n"
-      sudo apt --only-upgrade install $packageName
+      # using nala or apt
+      if ( command -v nala &> /dev/null )
+      then
+        clear
+        sudo nala install --update $packageName
+      else
+        clear
+        sudo apt update
+        sudo apt --only-upgrade install $packageName
+      fi
     done
+
+
+
+  # fedora
+  elif [ "$fedoraRedhat" ]
+  then
+    while :
+    do
+      clear
+
+      echo -e "\n${DMagenta}============= List Upgradable Packages =============${Color_Off}"
+      sudo dnf update --assumeno
+
+      echo -e "\n${DMagenta}=====================================================================================================${Color_Off}"
+      echo -e "Type the exact package name you want to upgrade according to the list."
+      echo -e "If you want to do multiple packages, just put a space after each one."
+      echo -e "\n${Green}Usually the package name is on the most left of the list${Color_Off}"
+      echo -e "\nExample : ${BRed}gimp kdenlive linux-firmware linux-headers-liquorix-amd64 linux-image-liquorix-amd64"
+      echo -e "${Green}Type ${BRed}exit ${Green}to exit this process."
+      echo -e "${DMagenta}=====================================================================================================${Color_Off}"
+
+      echo -ne "${DYellow}\nUpgrade Package : ${Color_Off}"
+      read packageName
+
+      if [ "$packageName" = "exit" ]
+      then
+        echo "Done."
+        break
+      fi
+
+      clear
+      sudo dnf upgrade $packageName
+    done
+
+
+
+  # arch
+  elif [ "$arch" ]
+  then
+    while :
+    do
+      clear
+
+      echo -e "\n${DMagenta}============= List Upgradable Packages =============${Color_Off}"
+      no | LC_ALL=en_US.UTF-8 sudo pacman -Syu
+
+      echo -e "\n${DMagenta}=====================================================================================================${Color_Off}"
+      echo -e "Type the exact package name you want to upgrade according to the ${BRed}Packages${ColorOff} section."
+      echo -e "If you want to do multiple packages, just put a space after each one."
+      echo -e "\n${Green}Please do not include the version number. ${BRed}Only type the package name !${Color_Off}"
+      echo -e "\nExample : ${BRed}gimp kdenlive linux-firmware linux-headers-liquorix-amd64 linux-image-liquorix-amd64"
+      echo -e "${Green}Type ${BRed}exit ${Green}to exit this process."
+      echo -e "${DMagenta}=====================================================================================================${Color_Off}"
+
+      echo -ne "${DYellow}\nUpgrade Package : ${Color_Off}"
+      read packageName
+
+      if [ "$packageName" = "exit" ]
+      then
+        echo "Done."
+        break
+      fi
+      
+      clear
+      echo -e "\n${DMagenta}============= Upgrading $packageName =============${Color_Off}\n"
+      sudo pacman -Sy $packageName
+    done
+
   else
-    echo -e "${Green}This option is only for debian based distros only."
-    echo -e "As far as I know only debian can do this.${Color_Off}"
+    echo -e "${Green}This option is only for debian, fedora, and arch based distros."
+    sleep 5
   fi
 }
 
-# check
+
+# -------------------------------------------------------------------------------------
+#                                 SCRIPT STARTS
+# -------------------------------------------------------------------------------------
 checkFedoraRedhat
 checkArch
 checkDebian
 checkSUSE
 
-# ----------------------------------------------------------------------------------
 HEIGHT=300
 WIDTH=900
 PROMPT="Install Upgrade"
@@ -83,7 +162,7 @@ COLUMN3="Distros"
 
 OPTIONS=(
   Upgrade\ All "upgrade all the packages" "works on fedora, debian, arch, and opensuse"
-  Choose\ Upgrade "choose what package or packages that you want to upgrade" "works only on debian/ubuntu"
+  Choose\ Upgrade "choose what package or packages that you want to upgrade" "works on fedora, debian, and arch"
   Skip "skip upgrading package" "-"
 )
 
