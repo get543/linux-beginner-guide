23a9c2f612be16e08f0ba6b44e60a3e322d6ae46
get543 <fghaza.pratama@gmail.com>
2023-03-13 13:19:21 +0700

    fix view things
    - fix error when not creating a folder
    - now can choose what is the drive's file type
    - add warning

--- change-perm.sh
+++ change-perm.sh
@@ -3,6 +3,8 @@
 sudo df -H &&
 
 echo -e "\nExample of USB Drive : /dev/sdb1"
+echo -e "If you don't see the drive you want, it probably isn't mounted already."
+echo -e "So, don't run the script and look at your disk utility."
 echo -ne "Which drive : "
 read drive
 
@@ -10,12 +12,19 @@ echo -e "\nExample of Mount Point Folder: USB STICK"
 echo -ne "Mount point name : "
 read mountPoint
 
+echo -e "Example of file type : ntfs, vfat, ext4"
+echo -ne "Drive file type : "
+read fileType
+
+if [[ ! $drive || ! $mountPoint || ! $fileType ]]; then
+  echo -e "\nYou need to fill out those questions !!"
+  return
+fi
+
 echo -e "\n"
 
-mkdir -vp "/media/$USER/$mountPoint"
+mkdir -p "/media/$USER/$mountPoint" &&
 
 sudo umount $drive &&
 
-sudo mount -t vfat -o rw,exec,uid=1000,gid=1000,umask=022 $drive "/media/$USER/$mountPoint" &&
-
-echo -e "Mounted $drive drive at /media/$USER/$mountPoint"
+sudo mount -t $fileType -o rw,exec,uid=1000,gid=1000,umask=022 $drive "/media/$USER/$mountPoint" &&
