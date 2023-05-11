e940eaba546ae98dfd47dacee525a6782887384b
get543 <fghaza.pratama@gmail.com>
2023-05-11 17:42:08 +0700

    New GUI for choose upgrade option !
    - debian : choose upgrade option, now uses zenity checkbox. supports both nala and apt.
    
    Minor changes :
    - change upgrade command for opensuse
    - separate function for choose upgrade
    
    * Note :
    * fedora and arch choose upgrade gui is still in planning phase

--- autoinstall/updates/upgrade.sh
+++ autoinstall/updates/upgrade.sh
@@ -12,7 +12,7 @@ upgradeALL() {
     sudo dnf upgrade
   elif [ "$opensuse" ]
   then
-    sudo zypper dup
+    sudo zypper update
   else
     echo -e "${Green}I'm sorry you're probably running distro other than.."
     echo -e "fedora/redhat, arch, debian/ubuntu, opensuse"
@@ -22,125 +22,126 @@ upgradeALL() {
   fi
 }
 
-chooseUpgrade() {
-  # debian
-  if [ "$debian" ]
+chooseUpgradeDebian() {
+  # nala is installed
+  if (command -v nala &> /dev/null )
   then
     while :
     do
-      clear
+      nalaList=$(command nala list --upgradable | awk 'NR%2==1' | awk '{print $1}' | tr -d '└──' | awk NF | awk '{print "FALSE", $1}')
 
-      echo -e "\n${DMagenta}============= List Upgradable Packages =============${Color_Off}"
+      menu=$(zenity --list \
+        --checklist \
+        --height=800 \
+        --width=900 \
+        --column "Checkbox" \
+        --column "Package Name" \
+        ${nalaList}
+      )
 
-      # using nala or apt
-      if ( command -v nala &> /dev/null )
+      choices="${menu[@]}"
+
+      final=$(echo "$choices" | tr "|" " ")
+
+      if [[ ! $final ]]
       then
-        nala list --upgradable
-      else
-        apt list --upgradable
-      fi
-
-      echo -e "\n${DMagenta}=====================================================================================================${Color_Off}"
-      echo -e "Type the exact package name you want to upgrade according to the list."
-      echo -e "If you want to do multiple packages, just put a space after each one."
-      echo -e "\n${Green}Usually the package name is before the slash (/)."
-      echo -e "${Green}If you are using ${BRed}nala ${Green}the package name usually have different colors.${Color_Off}"
-      echo -e "\nExample : ${BRed}gimp kdenlive linux-firmware linux-headers-liquorix-amd64 linux-image-liquorix-amd64"
-      echo -e "${Green}Type ${BRed}exit ${Green}to exit this process."
-      echo -e "${DMagenta}=====================================================================================================${Color_Off}"
-
-      echo -ne "${DYellow}\nUpgrade Package : ${Color_Off}"
-      read packageName
-
-      if [ "$packageName" = "exit" ]
-      then
-        echo "Done."
         break
-      fi
 
-      # using nala or apt
-      if ( command -v nala &> /dev/null )
-      then
-        clear
-        sudo nala install --update $packageName
       else
         clear
-        sudo apt update
-        sudo apt --only-upgrade install $packageName
+        sudo nala install --update --assume-yes $choices
+
       fi
-    done
-
-
-
-  # fedora
-  elif [ "$fedoraRedhat" ]
-  then
-    while :
-    do
-      clear
-
-      echo -e "\n${DMagenta}============= List Upgradable Packages =============${Color_Off}"
-      sudo dnf update --assumeno
-
-      echo -e "\n${DMagenta}=====================================================================================================${Color_Off}"
-      echo -e "Type the exact package name you want to upgrade according to the list."
-      echo -e "If you want to do multiple packages, just put a space after each one."
-      echo -e "\n${Green}Usually the package name is on the most left of the list${Color_Off}"
-      echo -e "\nExample : ${BRed}gimp kdenlive linux-firmware linux-headers-liquorix-amd64 linux-image-liquorix-amd64"
-      echo -e "${Green}Type ${BRed}exit ${Green}to exit this process."
-      echo -e "${DMagenta}=====================================================================================================${Color_Off}"
-
-      echo -ne "${DYellow}\nUpgrade Package : ${Color_Off}"
-      read packageName
-
-      if [ "$packageName" = "exit" ]
-      then
-        echo "Done."
-        break
-      fi
-
-      clear
-      sudo dnf upgrade $packageName
-    done
-
-
-
-  # arch
-  elif [ "$arch" ]
-  then
-    while :
-    do
-      clear
-
-      echo -e "\n${DMagenta}============= List Upgradable Packages =============${Color_Off}"
-      no | LC_ALL=en_US.UTF-8 sudo pacman -Syu
-
-      echo -e "\n${DMagenta}=====================================================================================================${Color_Off}"
-      echo -e "Type the exact package name you want to upgrade according to the ${BRed}Packages${ColorOff} section."
-      echo -e "If you want to do multiple packages, just put a space after each one."
-      echo -e "\n${Green}Please do not include the version number. ${BRed}Only type the package name !${Color_Off}"
-      echo -e "\nExample : ${BRed}gimp kdenlive linux-firmware linux-headers-liquorix-amd64 linux-image-liquorix-amd64"
-      echo -e "${Green}Type ${BRed}exit ${Green}to exit this process."
-      echo -e "${DMagenta}=====================================================================================================${Color_Off}"
-
-      echo -ne "${DYellow}\nUpgrade Package : ${Color_Off}"
-      read packageName
-
-      if [ "$packageName" = "exit" ]
-      then
-        echo "Done."
-        break
-      fi
-
-      clear
-      echo -e "\n${DMagenta}============= Upgrading $packageName =============${Color_Off}\n"
-      sudo pacman -Sy $packageName
     done
 
   else
-    echo -e "${Green}This option is only for debian, fedora, and arch based distros."
-    sleep 5
+    aptList=$(command apt list --upgradable | awk '{print $1}' | cut -d / -f 1 | tail -n +2 | awk '{print "FALSE", $1}')
+
+    menu=$(zenity --list \
+      --checklist \
+      --height=800 \
+      --width=900 \
+      --column "Checkbox" \
+      --column "Package Name" \
+      ${aptList}
+    )
+
+    choices="${menu[@]}"
+
+    final=$(echo "$choices" | tr "|" " ")
+
+    if [[ ! $final ]]
+    then
+      break
+    
+    else
+      clear
+      sudo apt update
+      sudo apt --only-upgrade install $packageName
+
+    fi
   fi
+}
+
+chooseUpgradeFedora() {
+  while :
+  do
+    clear
+
+    echo -e "\n${DMagenta}============= List Upgradable Packages =============${Color_Off}"
+    sudo dnf update --assumeno
+
+    echo -e "\n${DMagenta}=====================================================================================================${Color_Off}"
+    echo -e "Type the exact package name you want to upgrade according to the list."
+    echo -e "If you want to do multiple packages, just put a space after each one."
+    echo -e "\n${Green}Usually the package name is on the most left of the list${Color_Off}"
+    echo -e "\nExample : ${BRed}gimp kdenlive linux-firmware linux-headers-liquorix-amd64 linux-image-liquorix-amd64"
+    echo -e "${Green}Type ${BRed}exit ${Green}to exit this process."
+    echo -e "${DMagenta}=====================================================================================================${Color_Off}"
+
+    echo -ne "${DYellow}\nUpgrade Package : ${Color_Off}"
+    read packageName
+
+    if [ "$packageName" = "exit" ]
+    then
+      echo "Done."
+      break
+    fi
+
+    clear
+    sudo dnf upgrade $packageName
+  done
+}
+
+chooseUpgradeArch() {
+  while :
+  do
+    clear
+
+    echo -e "\n${DMagenta}============= List Upgradable Packages =============${Color_Off}"
+    no | LC_ALL=en_US.UTF-8 sudo pacman -Syu
+
+    echo -e "\n${DMagenta}=====================================================================================================${Color_Off}"
+    echo -e "Type the exact package name you want to upgrade according to the ${BRed}Packages${ColorOff} section."
+    echo -e "If you want to do multiple packages, just put a space after each one."
+    echo -e "\n${Green}Please do not include the version number. ${BRed}Only type the package name !${Color_Off}"
+    echo -e "\nExample : ${BRed}gimp kdenlive linux-firmware linux-headers-liquorix-amd64 linux-image-liquorix-amd64"
+    echo -e "${Green}Type ${BRed}exit ${Green}to exit this process."
+    echo -e "${DMagenta}=====================================================================================================${Color_Off}"
+
+    echo -ne "${DYellow}\nUpgrade Package : ${Color_Off}"
+    read packageName
+
+    if [ "$packageName" = "exit" ]
+    then
+      echo "Done."
+      break
+    fi
+
+    clear
+    echo -e "\n${DMagenta}============= Upgrading $packageName =============${Color_Off}\n"
+    sudo pacman -Sy $packageName
+  done
 }
 
 
@@ -173,9 +174,23 @@ clear
 
 # case
 case "$opt" in
-  Upgrade\ All) upgradeALL;;
+  Upgrade\ All) upgradeALL ;;
 
-  Choose\ Upgrade) chooseUpgrade ;;
+  Choose\ Upgrade)
+    if [ "$debian" ]
+    then
+      chooseUpgradeDebian
+    elif [ "$fedoraRedhat" ]
+    then
+      chooseUpgradeFedora
+    elif [ "$arch" ]
+    then
+      chooseUpgradeArch
+    else
+      echo -e "${Green}This option is only for debian, fedora, and arch based distros."
+      sleep 5
+    fi
+  ;;
 
   Skip) echo "Done." ;;
 
