5bb610495c9ddb14c865c6262f5fda2d7c65f2f3
get543 <fghaza.pratama@gmail.com>
2025-03-01 00:52:49 -0500

    check if command exist

--- autoinstall/autoupgrade.sh
+++ autoinstall/autoupgrade.sh
@@ -78,9 +78,11 @@ main() {
         read
     fi
 
-    flatpak update -y
-
-    pip --disable-pip-version-check list --outdated --format=json | python -c "import json, sys; print('\n'.join([x['name'] for x in json.load(sys.stdin)]))" | xargs -n1 pip install -U
+    if (command -v flatpak &>/dev/null); then
+        flatpak update -y
+    elif (command -v pip &>/dev/null); then
+        pip --disable-pip-version-check list --outdated --format=json | python -c "import json, sys; print('\n'.join([x['name'] for x in json.load(sys.stdin)]))" | xargs -n1 pip install -U
+    fi
 }
 
 # run main function
